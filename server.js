692b51089e213a5aa1798d0225eaeca64f09a92e
aik <anthonykrueger0@gmail.com>
2021-08-18 11:58:28 -0500

    basic routing and model added

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const path = require('path');
+
+const PORT = process.env.PORT || 3000
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.use(require('./routes'))
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {useNewUrlParser: true});
+
+app.listen(PORT, () => {
+    console.log("Listening!")
+})
